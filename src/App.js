fd4706ede36b72168e9476ef5e62be00c23dbe5d
Akshay <akshay@Akshays-MBP-2.attlocal.net>
2021-08-15 11:37:22 -0500

    learn react text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bye Bye Birdie
+          learn react
         </a>
       </header>
     </div>
